91d2bac8452b2a3f15c0590af3bc2a70ce54a924
Denice Montemayor <dmontema@1-D-15.42heilbronn.de>
2022-03-12 18:07:52 +0100

    create func to create a stringbuilder struct.

--- stringbuider.c
+++ stringbuider.c
@@ -6,9 +6,17 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 17:53:02 by dmontema          #+#    #+#             */
-/*   Updated: 2022/03/12 18:06:18 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/03/12 18:07:14 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/stringbuilder.h"
 
+t_stringbuilder	*sb_create()
+{
+	t_stringbuilder	*sb;
+
+	sb->str = NULL;
+	sb->len = 0;
+	return (sb);
+}
