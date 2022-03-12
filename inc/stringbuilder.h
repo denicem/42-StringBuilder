b1fc29c5effea202add9f2b46e8c236473a12bc7
Denice Montemayor <dmontema@1-D-15.42heilbronn.de>
2022-03-12 18:07:10 +0100

    init commit. prepared header file.

---
+++ inc/stringbuilder.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stringbuilder.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/12 17:47:05 by dmontema          #+#    #+#             */
+/*   Updated: 2022/03/12 18:00:33 by dmontema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRINGBUILDER_H
+# define STRINGBUILDER_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_stringbuilder
+{
+	char	*str;
+	int		len;
+}	t_stringbuilder;
+
+t_stringbuilder	*sb_create();
+int		sb_append_int(t_stringbuilder *sb, int nbr);
+int		sb_append_char(t_stringbuilder *sb, char c);
+int		sb_append_str(t_stringbuilder *sb, char *str);
+char	*sb_get_str(t_stringbuilder *sb);
+int		sb_destroy(t_stringbuilder *sb);
+
+# endif STRINGBUILDER_H
